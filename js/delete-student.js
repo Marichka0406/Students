7af2afd2d7ec033d63d15c801c7de06f6bd6665e
Анна-Марія <anna-mariia.kurhanevych.pz.2021@lpnu.ua>
2023-03-07 14:23:33 +0200

    Update

--- js/delete-student.js
+++ js/delete-student.js
@@ -6,6 +6,8 @@ let warningMessage=document.querySelector('.warning-message');
 //let userFirstName = document.querySelector('.first-name-input');
 //let userLastName = document.querySelector('.last-name-input');
 
+
+
 function deleteItem(event) {
   let deleteElement = event.target;
   let parent = deleteElement.parentNode.parentNode.parentNode;
@@ -18,16 +20,12 @@ function deleteItem(event) {
   });
 }
 
-
-
-
 let deleteButtons = document.querySelectorAll(".delete-button");
 deleteButtons.forEach(button => {
   button.addEventListener("click", deleteItem);
 });
 
 function cancelDelete() {
-  //button.deleteEventListener("click", deleteItem);
   warningContainer.style.display = "none";
   warningMessage.textContent=" ";
 }
